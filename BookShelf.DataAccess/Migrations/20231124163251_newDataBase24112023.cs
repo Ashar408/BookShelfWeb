20c362227e061ed6b158bcfcbbf6b3c655222dc5
Ashar Rasheed <Asharrasheed32@gmail.com>
2024-01-22 12:45:00 +0500

    Product View Changes

---
+++ BookShelf.DataAccess/Migrations/20231124163251_newDataBase24112023.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookShelf.DataAccess.Migrations
+{
+    public partial class newDataBase24112023 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
